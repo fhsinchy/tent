2af985e4052d9a6da7107167cbb12a4dd633f3b1
Farhan Hasin Chowdhury <mail@farhan.info>
2020-12-22 19:17:33 +0600

    Added  urfave/cli tool

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fhsinchy/tent
 
 go 1.15
 
-require github.com/containers/podman/v2 v2.2.1
+require (
+	github.com/containers/podman/v2 v2.2.1
+	github.com/urfave/cli/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/containers/podman/v2/libpod/define"
@@ -11,54 +12,65 @@ import (
 	"github.com/containers/podman/v2/pkg/bindings/images"
 	"github.com/containers/podman/v2/pkg/domain/entities"
 	"github.com/containers/podman/v2/pkg/specgen"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("the podman thingy")
+	app := &cli.App{
+		Action: func(c *cli.Context) error {
+			fmt.Println("the podman thingy")
 
-	// Get Podman socket location
-	sockDir := os.Getenv("XDG_RUNTIME_DIR")
-	socket := "unix:" + sockDir + "/podman/podman.sock"
+			if c.Args().Get(0) == "mysql" {
+				// Get Podman socket location
+				sockDir := os.Getenv("XDG_RUNTIME_DIR")
+				socket := "unix:" + sockDir + "/podman/podman.sock"
 
-	// Connect to Podman socket
-	connText, err := bindings.NewConnection(context.Background(), socket)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+				// Connect to Podman socket
+				connText, err := bindings.NewConnection(context.Background(), socket)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				rawImage := "docker.io/mysql:5.7"
+				fmt.Println("pulling mysql image")
+				_, err = images.Pull(connText, rawImage, entities.ImagePullOptions{})
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				env := make(map[string]string)
+				env["MYSQL_ROOT_PASSWORD"] = "secret"
+
+				// Container create
+				s := specgen.NewSpecGenerator(rawImage, false)
+				s.Env = env
+				r, err := containers.CreateWithSpec(connText, s)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				// Container start
+				fmt.Println("starting mysql container")
+				err = containers.Start(connText, r.ID, nil)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				running := define.ContainerStateRunning
+				_, err = containers.Wait(connText, r.ID, &running)
+				if err != nil {
+					log.Fatalln(err)
+				}
+			} else {
+				fmt.Println("service name is required")
+			}
+
+			return nil
+		},
 	}
 
-	rawImage := "docker.io/mysql"
-	fmt.Println("pulling mysql image")
-	_, err = images.Pull(connText, rawImage, entities.ImagePullOptions{})
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	env := make(map[string]string)
-	env["MYSQL_ROOT_PASSWORD"] = "secret"
-
-	// Container create
-	s := specgen.NewSpecGenerator(rawImage, false)
-	s.Env = env
-	r, err := containers.CreateWithSpec(connText, s)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Container start
-	fmt.Println("starting mysql container")
-	err = containers.Start(connText, r.ID, nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	running := define.ContainerStateRunning
-	_, err = containers.Wait(connText, r.ID, &running)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
